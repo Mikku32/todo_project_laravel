6eb6c5726511b7fb82d1bac0580b0655b198abf8
Mikdhad.k <mikdhadk32@gmail.com>
2024-08-27 16:46:08 +0530

    completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
     <div class="flex flex-col gap-4 px-3 mt-6 ">
         @foreach ($todos as $t)
             <div onclick="window.location='{{ route('todoDetails', ['todo' => $t->id]) }}'"
-                class="flex justify-between w-full rounded-xl py-2 overflow-hidden shadow-lg bg-gradient-to-r from-violet-200 to-fuchsia-200 hover:from-violet-400 hover:to-fuchsia-400">
+                class="flex justify-between items-center w-full rounded-xl py-2 overflow-hidden shadow-lg bg-gradient-to-r from-violet-200 to-fuchsia-200 hover:from-violet-400 hover:to-fuchsia-400">
                 <div class="flex items-center px-6 pt-4">
                     <input type="checkbox" class="form-checkbox h-5 w-5 text-gray-600" {{ $t->is_completed ? 'checked' : '' }}>
                     <div class="ml-6">
@@ -22,14 +22,14 @@
                     </div>
                 </div>
 
-                <div class="py-4  items-center flex justify-end">
+                <div class="py-4  items-center flex flex-col sm:flex-row justify-end">
                     <a href="{{ route('todoUpdate', ['todo' => $t->id]) }}"
                         class="text-blue-700  h-9 hover:text-blue-900 hover:underline font-semibold  py-2 rounded">Edit</a>
                     <form action="{{ route('todoDelete', ['todo' => $t->id]) }}" method="POST">
                         @csrf
                         @method('DELETE')
                         <button type="submit"
-                            class="text-red-700 px-4 h-9 hover:text-red-900 hover:underline font-semibold  py-2 rounded">Delete</button>
+                            class="text-red-700 px-4 h-9 hover:text-red-900 hover:underline font-semibold  sm:py-2 rounded">Delete</button>
                     </form>
                 </div>
             </div>
