deb073745f8695b348d7b247f98de0a33553ff00
Mikdhad.k <mikdhadk32@gmail.com>
2024-08-27 11:34:22 +0530

    home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,20 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="  py-8  min-h-screen bg-slate-100">
-    <h1 class="text-4xl font-bold mb-2">Todo</h1>
-    <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        @foreach ($todos as $todo)
-            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-gradient-to-r from-violet-200 to-fuchsia-200">
-                <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">{{ $todo->title }}</div>
-                    <p class="text-gray-700 text-base">{{ $todo->description }}</p>
+<div class="  py-8 px-4 min-h-screen bg-slate-300">
+    <h1 class="text-4xl font-bold mb-4">Todos</h1>
+    <div class="flex flex-col gap-4 px-3 ">
+        @foreach ($todos as $t)
+            <div class="w-full rounded overflow-hidden shadow-lg bg-gradient-to-r from-violet-200 to-fuchsia-200">
+                <div class="flex items-center px-6 pt-4">
+                    <input type="checkbox" class="form-checkbox h-5 w-5 text-gray-600">
+                    <div class="ml-6">
+                        <div class="font-bold text-xl mb-1">{{ $t->title }}</div>
+                        <p class="text-gray-700 text-base">{{ $t->description }}</p>
+                    </div>
                 </div>
 
-                <div class="px-6 pt-4 pb-2 flex justify-end">
+                <div class="px-6  pb-2 flex justify-end">
                     <a href="" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</a>
                 </div>
             </div>
